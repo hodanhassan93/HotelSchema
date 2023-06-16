2d78cb51e65d7781b02793289b2077a9250c144a
Eleanor Maitland <maitland.eleanor@gmail.com>
2023-06-16 16:00:51 +0100

    final commit

--- Team1-HotelQueries.sql
+++ Team1-HotelQueries.sql
@@ -1,35 +1,33 @@
 -- 1. Write a query that returns a list of reservations that end in July 2023, including the name of the guest, the room number(s), and the reservation dates.
-select g.name 'guest_name', r.room_num, r.start_date, r.end_date
-from reservation_details r
-join guests g on r.guest_Id = g.guest_Id
-where r.end_date >= '2023-07-01' and r.end_date <= '2023-07-31';
--- 1. Write a query that returns a list of reservations that end in July 2023, including the name of the guest, the room number(s), and the reservation dates.
 select g.name 'guest_name', rd.room_num, rd.start_date, rd.end_date
-from Reservation_details rd
-join Final_res fr on rd.reservation_Id = fr.reservation_Id
-join guests g on fr.guest_res_Id = g.guest_Id
-where rd.end_date >= '2023-07-01' and rd.end_date <= '2023-07-31';
+from guests g
+join guest_res gr on g.guest_id = gr.guest_id
+join final_res fr on gr.guest_res_id = fr.guest_res_id
+join reservation_details rd on fr.reservation_id = rd.reservation_id
+where rd.end_date >= '2023-07-01' and rd.end_date < '2023-08-01';
+
 -- 2. Write a query that returns a list of all reservations for rooms with a jacuzzi, displaying the guest's name, the room number, and the dates of the reservation.
-select g.name 'guest_name', rd.room_num, rd.start_date, rd.end_date
-from Reservation_details rd
-join Final_res fr on rd.reservation_Id = fr.reservation_Id
-join guests g on fr.guest_res_Id = g.guest_Id
-join Room r on rd.room_num = r.room_num
+select g.name "guest_name", rd.room_num, rd.start_date, rd.end_date
+from guests g
+join guest_res gr on g.guest_id = gr.guest_id
+join final_res fr on gr.guest_res_id = fr.guest_res_id
+join reservation_details rd on fr.reservation_id = rd.reservation_id
+join room r on rd.room_num = r.room_num
 join room_amenity ra on r.room_num = ra.room_num
-join amenity a on ra.amenities_Id = a.amenities_Id
-where a.amenities_name = 'jacuzzi';
+join amenity a on ra.amenities_id = a.amenities_id
+where a.amenities_name = 'Jacuzzi';
 
 -- 3. Write a query that returns all the rooms reserved for a specific guest, 
 -- including the guest's name, the room(s) reserved, the starting date of the reservation, 
 -- and how many people were included in the reservation. (Choose a guest's name from the existing data.)
 
-SELECT G.name, RD.start_date, R.room_num, GR.adults + GR.children "total_people"
-FROM Guests G
-JOIN guest_res GR ON G.guest_Id = GR.guest_Id
-JOIN Final_res FR ON GR.guest_res_Id = FR.guest_res.guest_Id
-JOIN Reservation_details RD ON FR.reservation_Id = RD.reservation_Id
-JOIN Room R ON RD.room_num = R.room_num
-WHERE G.name = "Aurore Lipton";
+SELECT G.name, RD.start_date, R.room_num, GR.adults + GR.children AS total_people
+FROM guests G
+JOIN guest_res GR ON G.guest_id = GR.guest_id
+JOIN final_res FR ON GR.guest_res_Id = FR.guest_res_Id
+JOIN reservation_details RD ON FR.reservation_Id = RD.reservation_id
+JOIN room R ON RD.room_num = R.room_num
+WHERE G.name = 'Aurore Lipton';
 
 -- 4. Write a query that returns a list of rooms, reservation ID, and per-room cost for each reservation. 
 -- The results should include all rooms, whether or not there is a reservation associated with the room.
@@ -55,25 +53,27 @@ LEFT JOIN
 GROUP BY
     r.room_num,
     fr.reservation_Id,
+    rd.total_cost,
     rt.base_price,
     gr.adults,
     gr.children;
 
 -- 5) Write a query that returns all rooms with a capacity of three or more and that are reserved on any date in April 2023.
-select room_num
-from reservation_detail
-where start_date = "%-04-%" OR end_date = "%-04-%";
-
+select room.room_num
+from room
+right join room_type on room.type_Id=room_type.type_Id
+right join reservation_details on room.room_num=reservation_details.room_num
+where (max_occ >=3) AND (MONTH(start_date) = 4 OR MONTH(end_date) = 4 AND YEAR(start_date) = 2023);
 
 -- 6)Write a query that returns a list of all guest names and the number of reservations per guest, sorted starting with the guest with the most reservations and then by the guest's last name.
-select guest_name, count(guest_res.guest_Id) 
-from Guests
+select name, count(guest_res.guest_Id) 
+from guests
 inner join guest_res 
-on Guests.guest_Id = guest_res.guest_Id
-group by Guests.guest_Id
-order by Guests.guest_Id;
+on guests.guest_Id = guest_res.guest_Id
+group by guests.guest_Id
+order by guests.guest_Id;
 
 -- 7) Write a query that displays the name, address, and phone number of a guest based on their phone number. (Choose a phone number from the existing data.)
-select guest_name, address, phone
-from Guests
+select name, address, phone
+from guests
 where phone = "(446) 396-6785";
